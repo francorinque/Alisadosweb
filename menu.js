d7cfc8747a7a654f60f9d2ee5d857d9aa5c37e6d
francorinque <francoagustinrinquecorreo@gmail.com>
2022-06-19 19:22:15 -0300

    correcióndelinks

--- menu.js
+++ menu.js
@@ -1,11 +1,14 @@
-const showmenu = document.querySelector('.nav__icon');
-const links = document.querySelector('.nav__menu');
+const icon = document.querySelector(".nav__icon");
+const menu = document.querySelector(".nav__menu");
 
 
-
-
-showmenu.addEventListener('click', () =>{
-    links.classList.toggle('active');
-    showmenu.classList.toggle('equis');
-
+icon.addEventListener('click',() =>{
+    icon.classList.toggle("active");
+    menu.classList.toggle("active");
 })
+
+
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+    icon.classList.remove("active");
+    menu.classList.remove("active");
+}));
